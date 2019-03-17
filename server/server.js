85ecd3bf9bf57cc248478e633fb7f7097d28c8ec
Oliver Portee <oliver.portee@gmail.com>
2019-03-17 13:59:49 +0100

    set up eslint to check the coding style

--- server/server.js
+++ server/server.js
@@ -7,18 +7,13 @@ const bodyparser = require('body-parser');
 const app = express();
 
 // middleware for parsing and simplifying incoming http requests
-app.use(bodyparser.urlencoded({extended: false}));
+app.use(bodyparser.urlencoded({ extended: false }));
 app.use(bodyparser.json());
 
-app.get('/', function(req, res) {
+app.get('/', (req, res) => {
   res.send('<h1>Server is running</h1>');
 });
 
 // server listens on port 5000
 const port = 5000;
 app.listen(port, () => console.log('Server is listening on port 5000'));
-
-
-
-
-
