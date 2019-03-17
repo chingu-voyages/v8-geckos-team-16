0322b37578e519f006d3449d906f8e3050cd115c
Oliver Portee <oliver.portee@gmail.com>
2019-03-17 12:38:46 +0100

    set up a simple express server

--- server/server.js
+++ server/server.js
@@ -1,3 +1,24 @@
 
 
+const express = require('express');
+const bodyparser = require('body-parser');
+
+
+const app = express();
+
+// middleware for parsing and simplifying incoming http requests
+app.use(bodyparser.urlencoded({extended: false}));
+app.use(bodyparser.json());
+
+app.get('/', function(req, res) {
+  res.send('<h1>Server is running</h1>');
+});
+
+// server listens on port 5000
+const port = 5000;
+app.listen(port, () => console.log('Server is listening on port 5000'));
+
+
+
+
 
