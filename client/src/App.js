63c5b090475a3b5cd1795f9091c6ec15df4a1f11
Oliver Portee <oliver.portee@gmail.com>
2019-03-30 15:34:25 +0100

    removed unnecessary template code

--- client/src/App.js
+++ client/src/App.js
@@ -1,13 +1,10 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
 import './App.css';
 
 class App extends Component {
   render() {
     return (
-      <div>
-        <h1>This is the app</h1>
-      </div>
+      
     );
   }
 }
