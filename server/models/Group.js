d7f7f1cb4e9411c12760fa38d8bfdbc67efda63b
Oliver Portee <oliver.portee@gmail.com>
2019-04-02 11:55:42 +0200

    create mongoose schema for groups

--- server/models/Group.js
+++ server/models/Group.js
@@ -1,2 +1,79 @@
 
+const mongoose = require('mongoose');
 
+const GroupSchema = new mongooe.Schema({
+  name: {
+    type: String,
+    required: true,
+    unique: true
+  },
+  description: {
+    type: String,
+    required: true
+  },
+  members: {
+    type: [{
+      type: Schema.Types.ObjectId,
+      ref: 'User'
+    }],
+    required: true
+  },
+  admins: {
+    type: [{
+      type: Schema.Types.ObjectId,
+      ref: 'User'
+    }],
+    required: true
+  },
+  image: {
+    type: String,
+    required: false
+  },
+  upcomingEvents: {
+    type: [{
+      date: {
+        type: Date,
+        required: true,
+      },
+      location: {
+        type: {
+          street: {
+            type: String,
+            required: true
+          },
+          number: {
+            type: String,
+            required: true
+          },
+          zip: {
+            type: String,
+            required: true
+          },
+          city: {
+            type: String,
+            required: true
+          },
+          country: {
+            type: String,
+            required: true
+          }
+        },
+        required: true
+      },
+      description: {
+        type: String,
+        required: true
+      },
+      members: {
+        type: [{
+          type: Schema.Types.ObjectId,
+          ref: 'User'
+        }],
+        required: true
+      }
+    }],
+    required: true
+  }
+}
+
+export default const Group = mongoose.model('Group', GroupSchema);
